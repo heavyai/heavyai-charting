2a0dfce33e235bf9c16e9444b36380dff009fe26
Todd Mostak <tmostak@gmail.com>
2015-02-11 01:46:09 -0800

    Count widget works but may need a little tweaking on nice sizing for all cell sizes

--- js/dc.mapd.js
+++ js/dc.mapd.js
@@ -741,7 +741,7 @@ dc.baseMixin = function (_chart) {
     };
 
     var _data = function (group) {
-        
+        console.log("fetching"); 
         return group.all();
         //return group.top(10);
     };
