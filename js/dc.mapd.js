0cad6ab5df9f8bbc437e111d7da980be75c02e34
Todd Mostak <tmostak@gmail.com>
2015-04-24 20:35:47 -0700

    Beginnings of mapbox map support on client

--- js/dc.mapd.js
+++ js/dc.mapd.js
@@ -2000,6 +2000,48 @@ dc.colorMixin = function (_chart) {
     return _chart;
 };
 
+dc.mapMixin = function (_chart) {
+
+    function zoomHandler() {
+      console.log("zoom");
+      _chart._invokeZoomedListener();
+    }
+
+    var _zoom = d3.behavior.zoom().on('zoom', zoomHandler);
+    var _mouseZoomable = true;
+    var _hasBeenMouseZoomable = true;
+
+    function configureMouseZoom () {
+        if (_mouseZoomable) {
+            _chart._enableMouseZoom();
+        }
+        else if (_hasBeenMouseZoomable) {
+            _chart._disableMouseZoom();
+        }
+    }
+
+    _chart._enableMouseZoom = function () {
+        _hasBeenMouseZoomable = true;
+        _zoom.x(_chart.x())
+            .scaleExtent(_zoomScale)
+            .size([_chart.width(), _chart.height()])
+            .duration(_chart.transitionDuration());
+        _chart.root().call(_zoom);
+    };
+
+    configureMouseZoom();
+
+    return _chart;
+};
+
+dc.mapChart = function(parent, chartGroup) {
+  //var _chart = dc.mapMixin(dc.baseMixin({}));
+  var _chart = dc.coordinateGridMixin({});
+
+  return _chart.anchor(parent, chartGroup);
+}
+
+
 /**
 ## Coordinate Grid Mixin
 Includes: [Color Mixin](#color-mixin), [Margin Mixin](#margin-mixin), [Base Mixin](#base-mixin)
