a9f934625171faa1127d505f1738ced7edc7de51
Marc Balaban <marc@marcbalaban.com>
2015-08-12 17:19:53 -0700

    can now manipulate dom and using ReactIgnore to not look at children

--- js/dc.mapd.js
+++ js/dc.mapd.js
@@ -16,6 +16,7 @@
  *  limitations under the License.
  */
 
+
 (function() { function _dc(d3, crossfilter) {
 'use strict';
 
@@ -9578,23 +9579,38 @@ dc.stackableChart = dc.stackMixin;
 
 // Expose d3 and crossfilter, so that clients in browserify
 // case can obtain them if they need them.
+
 dc.d3 = d3;
 dc.crossfilter = crossfilter;
 
 return dc;}
     if(typeof define === "function" && define.amd) {
-        define(["d3", "crossfilter"], _dc);
-    } else if(typeof module === "object" && module.exports) {
         var _d3 = require('d3');
-        var _crossfilter = require('crossfilter');
+        var _crossfilter = require('./crossfilter.mapd.js');
         // When using npm + browserify, 'crossfilter' is a function,
         // since package.json specifies index.js as main function, and it
         // does special handling. When using bower + browserify,
         // there's no main in bower.json (in fact, there's no bower.json),
         // so we need to fix it.
+
         if (typeof _crossfilter !== "function") {
             _crossfilter = _crossfilter.crossfilter;
         }
+        
+        module.exports = _dc(_d3, _crossfilter);
+    } else if(typeof module === "object" && module.exports) {
+        var _d3 = require('d3');
+        var _crossfilter = require('./crossfilter.mapd.js');
+        // When using npm + browserify, 'crossfilter' is a function,
+        // since package.json specifies index.js as main function, and it
+        // does special handling. When using bower + browserify,
+        // there's no main in bower.json (in fact, there's no bower.json),
+        // so we need to fix it.
+
+        if (typeof _crossfilter !== "function") {
+            _crossfilter = _crossfilter.crossfilter;
+        }
+        
         module.exports = _dc(_d3, _crossfilter);
     } else {
         this.dc = _dc(d3, crossfilter);
