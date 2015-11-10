96ae75f41460f237e4cce7d24fbdc3cf9b7232de
Todd Mostak <tmostak@gmail.com>
2015-11-09 17:04:49 -0800

    Variable size point scale working for when there is no size variable - need to un-couple from sampling

--- main/source/js/dc-mapd/src/core.js
+++ main/source/js/dc-mapd/src/core.js
@@ -163,7 +163,6 @@ dc.renderAll = function (group) {
 
 /* OVERRIDE ---------------------------------------------------------------- */
         if (dc._sampledCount > 0) {
-          console.log(dc._sampledCount);
 
             // relies on count chart being first -- bad
             if (charts[i].isCountChart())
