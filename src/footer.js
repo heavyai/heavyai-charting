121d42989cd6112d0a86d31734981226f94cb6ce
Danny Delott <dannydelott@gmail.com>
2015-11-24 15:33:18 -0800

    [wip] Update footer with mapd overrides
    - Will need to change the imports once crossfilter is a private NPM module.

--- src/footer.js
+++ src/footer.js
@@ -15,10 +15,34 @@ dc.crossfilter = crossfilter;
 
 return dc;}
     if(typeof define === "function" && define.amd) {
-        define(["d3", "crossfilter"], _dc);
+        // define(["d3", "crossfilter"], _dc);
+
+/* OVERRIDE -----------------------------------------------------------------*/
+        var _d3 = require('d3');
+        var _crossfilter = require('./crossfilter.mapd.js');
+        // When using npm + browserify, 'crossfilter' is a function,
+        // since package.json specifies index.js as main function, and it
+        // does special handling. When using bower + browserify,
+        // there's no main in bower.json (in fact, there's no bower.json),
+        // so we need to fix it.
+/* --------------------------------------------------------------------------*/
+
+        if (typeof _crossfilter !== "function") {
+            _crossfilter = _crossfilter.crossfilter;
+        }
+
+/* OVERRIDE -----------------------------------------------------------------*/
+        module.exports = _dc(_d3, _crossfilter);
+/* --------------------------------------------------------------------------*/
+
     } else if(typeof module === "object" && module.exports) {
         var _d3 = require('d3');
-        var _crossfilter = require('crossfilter');
+        // var _crossfilter = require('crossfilter');
+
+/* OVERRIDE -----------------------------------------------------------------*/
+        var _crossfilter = require('./crossfilter.mapd.js');
+/* --------------------------------------------------------------------------*/
+
         // When using npm + browserify, 'crossfilter' is a function,
         // since package.json specifies index.js as main function, and it
         // does special handling. When using bower + browserify,
