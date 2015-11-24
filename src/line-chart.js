e8a6ba066e4a18481bac83e1ea7a2dbb6a9dbc16
Danny Delott <dannydelott@gmail.com>
2015-11-24 15:58:52 -0800

    Update line-chart with mapd overrides

--- src/line-chart.js
+++ src/line-chart.js
@@ -44,6 +44,11 @@ dc.lineChart = function (parent, chartGroup) {
     var _dashStyle;
     var _xyTipsOn = true;
 
+/* OVERRIDE ---------------------------------------------------------------- */
+    _chart.accent = accentLine;
+    _chart.unAccent = unAccentLine;
+/* ------------------------------------------------------------------------- */
+
     _chart.transitionDuration(500);
     _chart._rangeBandPadding(1);
 
@@ -248,12 +253,37 @@ dc.lineChart = function (parent, chartGroup) {
         }
     }
 
+/* OVERRIDE EXTEND --------------------------------------------------------- */
+    function accentLine (value) {
+      var chartDomain = _chart.x().domain();
+      var position = (value.getTime() - chartDomain[0].getTime()) / (chartDomain[1].getTime() - chartDomain[0].getTime());
+      var chartWidth = _chart.width() - _chart.margins().left - _chart.margins().right;
+      var xPixel = Math.floor(chartWidth * position) + _chart.margins().left;
+      //var xPixel = Math.floor(chartWidth * position);
+      this.svg()
+        .append("line")
+        .attr("class","accented")
+        .attr("x1", xPixel)
+        .attr("y1", 0 + _chart.margins().top)
+        .attr("x2", xPixel)
+        .attr("y2", _chart.height() - _chart.margins().bottom)
+    }
+
+    function unAccentLine (value) {
+      $("line.accented", this.chart).remove();
+    }
+/* ------------------------------------------------------------------------- */
+
     function safeD (d) {
         return (!d || d.indexOf('NaN') >= 0) ? 'M0,0' : d;
     }
 
     function drawDots (chartBody, layers) {
-        if (!_chart.brushOn() && _chart.xyTipsOn()) {
+
+/* OVERRIDE ---------------------------------------------------------------- */
+        // if (!_chart.brushOn() && _chart.xyTipsOn()) {
+/* ------------------------------------------------------------------------- */
+
             var tooltipListClass = TOOLTIP_G_CLASS + '-list';
             var tooltips = chartBody.select('g.' + tooltipListClass);
 
@@ -306,7 +336,11 @@ dc.lineChart = function (parent, chartGroup) {
 
                 dots.exit().remove();
             });
-        }
+
+/* OVERRIDE ---------------------------------------------------------------- */
+        // }
+/* ------------------------------------------------------------------------- */
+
     }
 
     function createRefLines (g) {
