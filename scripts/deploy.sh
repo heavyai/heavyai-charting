b1baf855e19c42fab2455ba98db4c810a828eafa
Danny Delott <dannydelott@gmail.com>
2015-12-03 15:11:59 -0800

    Refactor deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
+# show the command being run
+set -x
+
+# tell jenkins to fail if any of the commands fail
+set -e
+
 # If the last commit was from jenkins, do nothing
 if [[ `git log -1 | grep "Jenkins MapD"` ]]; then
-
  echo "Ignoring superfluous build spawned by jenkins"
-
-else
-
-  # remove the previous temp branch
-  echo "Removing the old temp branch"
-  git branch -d temp
-
-  # checkout a new temp branch
-  echo "Checking out a new temp branch"
-  git checkout -b temp
-
-  # bump the version
-  # TODO: set major, minor, or patch some how
-  echo "Bumping the version number"
-  npm --no-git-tag-version version patch
-
-  # push the new version to github
-  echo "Pushing up to master"
-  git push origin temp:master
-
-  # publish the module to npm
-  echo "Publishing to npm"
-  npm publish
-
-  echo "Done!"
-
+ exit 0
 fi
+
+# remove the previous temp branch
+git branch -d temp
+
+# checkout a new temp branch
+git checkout -b temp
+
+# bump the version
+# TODO: set major, minor, or patch some how
+npm --no-git-tag-version version patch
+
+# push the new version to github
+git push origin temp:master
+
+# publish the module to npm
+npm publish
 
