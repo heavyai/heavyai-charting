88ed9ba673aa304a7325266b286bd1bdc8567528
Jenkins MapD <machines@mapd.com>
2016-01-04 16:22:59 -0800

    0.1.24

--- mapdc.js
+++ mapdc.js
@@ -1,5 +1,5 @@
 /*!
- *  dc 0.1.23
+ *  dc 0.1.24
  *  http://dc-js.github.io/dc.js/
  *  Copyright 2012-2015 Nick Zhu & the dc.js Developers
  *  https://github.com/dc-js/dc.js/blob/master/AUTHORS
@@ -29,7 +29,7 @@
  * such as {@link #dc.baseMixin+svg .svg} and {@link #dc.coordinateGridMixin+xAxis .xAxis},
  * return values that are chainable d3 objects.
  * @namespace dc
- * @version 0.1.23
+ * @version 0.1.24
  * @example
  * // Example chaining
  * chart.width(300)
@@ -38,7 +38,7 @@
  */
 /*jshint -W079*/
 var dc = {
-    version: '0.1.23',
+    version: '0.1.24',
     constants: {
         CHART_CLASS: 'dc-chart',
         DEBUG_GROUP_CLASS: 'debug',
