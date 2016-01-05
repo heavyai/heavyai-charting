e53e65e02c437b8217e89c2201d4fbfee11fb3e5
Danny Delott <dannydelott@gmail.com>
2016-01-05 13:19:36 -0800

    Update crossfilter dependency

--- src/footer.js
+++ src/footer.js
@@ -18,7 +18,7 @@ return dc;}
 
 /* OVERRIDE -----------------------------------------------------------------*/
         var _d3 = require('d3');
-        var _crossfilter = require('./crossfilter.mapd.js');
+        var _crossfilter = require('@mapd/mapd-crossfilter');
         // When using npm + browserify, 'crossfilter' is a function,
         // since package.json specifies index.js as main function, and it
         // does special handling. When using bower + browserify,
@@ -38,7 +38,7 @@ return dc;}
         var _d3 = require('d3');
 
 /* OVERRIDE -----------------------------------------------------------------*/
-        var _crossfilter = require('./crossfilter.mapd.js');
+        var _crossfilter = require('@mapd/mapd-crossfilter');
 /* --------------------------------------------------------------------------*/
 
         // When using npm + browserify, 'crossfilter' is a function,
