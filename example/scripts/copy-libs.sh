7ee71c6b56f7baacdcfab6c1324bf9fbf911d9be
Danny Delott <dannydelott@gmail.com>
2016-04-12 10:56:30 -0700

    Update example asset paths, move build process
    - Run `npm build:example` from mapdc's package.json

--- example/scripts/copy-libs.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p js/
-cp ../mapdc.min.css css/
-cp ../chart.min.css css/
-cp ../mapdc.js js/
-cp ../node_modules/@mapd/mapd-crossfilter/mapd-crossfilter.js js/
---
+++ scripts/copy-example-libs.sh
@@ -0,0 +1,14 @@
+mkdir -p example/js/
+cp node_modules/lodash/lodash.js example/js
+cp node_modules/jquery/dist/jquery.min.js example/js
+cp node_modules/d3/d3.min.js example/js
+cp mapdc.min.css example/css/
+cp chart.min.css example/css/
+cp mapdc.js example/js/
+cp ../mapd-crossfilter/mapd-crossfilter.js example/js/
+cp ../mapd-con/dist/thrift.js example/js/
+cp ../mapd-con/dist/mapd.thrift.js example/js/
+cp ../mapd-con/dist/mapd_types.js example/js/
+cp ../mapd-con/dist/MapdCon.js example/js/
+cp ../mapd-mapbox-gl-js/dist/mapbox-gl.js example/js/
+cp ../mapd-mapbox-gl-js/dist/mapboxgl-overrides.js example/js/
