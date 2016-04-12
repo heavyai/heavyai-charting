8e74aa8fcd202a93eb22f003af9f71552cf17a8f
Danny Delott <dannydelott@gmail.com>
2016-04-11 17:31:18 -0700

    Add build_output.txt to libaries build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,7 @@
-./node_modules/grunt-cli/bin/grunt
+./node_modules/grunt-cli/bin/grunt | tee build_output.txt
+grep -q -i 'Done, without errors.' build_output.txt
+if [ "$?" == "0" ]; then
+    rm build_output.txt
+    exit 0
+fi
+exit 1
