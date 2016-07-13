2520bd25ddde9d0162d17db442cda5119abd2981
Jonathan Huang <grokut@gmail.com>
2016-07-12 19:21:41 -0700

    [COUNT CHART] use async methods, in naive way

--- src/count-widget.js
+++ src/count-widget.js
@@ -25,48 +25,41 @@ dc.countWidget = function (parent, chartGroup) {
         group.valueAsync(callbacks);
     });
 
-    _chart._doRender = function () {
-        var tot = _chart.dimension().size();
-        var val = null;
-        
-        if (_chart.dataCache != null)
-            val = _chart.dataCache;
-        else{
-            val = _chart.group().value();
-        }
+    _chart._doRender = function (val) {
+        _chart.dimension().size(function(err, tot) {
+            var all = _formatNumber(tot);
+            var selected = _formatNumber(val);
 
-        var all = _formatNumber(tot);
-        var selected = _formatNumber(val);
-        
-        var wrapper = _chart.root()
-            .style('width', 'auto')
-            .style('height', 'auto')
-            .html('')
-            .append('div')
-            .attr('class', 'count-widget');
+            var wrapper = _chart.root()
+                .style('width', 'auto')
+                .style('height', 'auto')
+                .html('')
+                .append('div')
+                .attr('class', 'count-widget');
 
-        wrapper.append('span')
-            .attr('class', 'count-selected')
-            .classed('not-filtered', selected === all)
-            .text(selected === '-0' ? 0 : selected);
-        
-        wrapper.append('span')
-            .classed('not-filtered', selected === all)
-            .text(' of ');
+            wrapper.append('span')
+                .attr('class', 'count-selected')
+                .classed('not-filtered', selected === all)
+                .text(selected === '-0' ? 0 : selected);
 
-        wrapper.append('span')
-            .attr('class', 'count-all')
-            .text(all);
+            wrapper.append('span')
+                .classed('not-filtered', selected === all)
+                .text(' of ');
 
-        wrapper.append('span')
-            .attr('class', 'count-label')
-            .text(' ' + _countLabel);
+            wrapper.append('span')
+                .attr('class', 'count-all')
+                .text(all);
+
+            wrapper.append('span')
+                .attr('class', 'count-label')
+                .text(' ' + _countLabel);
+        });
 
         return _chart;
     };
 
-    _chart._doRedraw = function () {
-        return _chart._doRender();
+    _chart._doRedraw = function (val) {
+        return _chart._doRender(val);
     };
 
     return _chart.anchor(parent, chartGroup);
