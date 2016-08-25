d4f1754556b80e99dab2791b77f7bb6204e5b38a
Du Hoang <du.hoang@gmail.com>
2016-08-24 17:33:14 -0700

    fix scatterplot filtering data by key0

--- src/bubble-chart.js
+++ src/bubble-chart.js
@@ -355,7 +355,7 @@ dc.bubbleChart = function (parent, chartGroup) {
         var bubbleG = _chart.chartBodyG().selectAll('g.' + _chart.BUBBLE_NODE_CLASS)
 
 /* OVERRIDE -----------------------------------------------------------------*/
-            .data(_chart.data(), function (d) { return d.key0; });
+            .data(_chart.data());
 /* --------------------------------------------------------------------------*/
 
         if (_sortBubbleSize) {
