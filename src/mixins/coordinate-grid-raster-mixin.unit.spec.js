78afdb0d7220a2145830b3b364ee7162b81eefd1
Tai Dupree <tdupreetan@gmail.com>
2017-07-09 17:18:30 -0700

    add test

--- src/mixins/coordinate-grid-raster-mixin.unit.spec.js
+++ src/mixins/coordinate-grid-raster-mixin.unit.spec.js
@@ -2,15 +2,16 @@ import {expect} from "chai"
 import coordinateGridRasterMixin from "./coordinate-grid-raster-mixin"
 
 describe("coordinateGridRasterMixin", () => {
-  let chart
-  
-  beforeEach(()=>{
-    chart = {}
-  })
-  
+  let chart = {};
+
   describe("constructor", () => {
     it('should construct Coordinate Grid Raster Mixin', () => {
       coordinateGridRasterMixin(chart)
+    })
+  })
+  describe("filters", () => {
+    it("should a filters method", () => {
+      expect(chart.filters().length).to.eq(0)
     })
   })
 })
