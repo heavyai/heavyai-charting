e0f41a30c7094d30ecd32c17152ade5daffeccf1
Marc Balaban <marc@marcbalaban.com>
2017-07-19 18:04:56 -0700

    [table] fixed custom selector parsing issue on table chart (#72)

--- src/charts/mapd-table.unit.spec.js
+++ src/charts/mapd-table.unit.spec.js
@@ -1,5 +1,6 @@
 import {expect} from "chai"
 import * as dc from "../index"
+import {splitStrOnLastAs} from "./mapd-table"
 
 describe("MapD Table Chart", () => {
   describe("constructor", () => {
@@ -7,6 +8,18 @@ describe("MapD Table Chart", () => {
       const node = window.document.createElement("DIV")
       const number = dc.mapdTable(node)
       expect(number.anchor()).to.equal(node)
+    })
+  })
+  describe("splitStrOnLastAs", () => {
+    it('should properly split on last AS within SQL statement', () => {
+      const sqlStatement = "cast(SUM(CASE WHEN Shot_result = 'made' THEN 1 ELSE 0 END) as float)/(cast(SUM(CASE WHEN Shot_result = 'missed' THEN 1 ELSE 0 END) as float) + cast(SUM(CASE WHEN Shot_result = 'made' THEN 1 ELSE 0 END) as float)) as col3"
+      const result = splitStrOnLastAs(sqlStatement)
+      expect(result).to.deep.equal(
+        [
+          "cast(SUM(CASE WHEN Shot_result = 'made' THEN 1 ELSE 0 END) as float)/(cast(SUM(CASE WHEN Shot_result = 'missed' THEN 1 ELSE 0 END) as float) + cast(SUM(CASE WHEN Shot_result = 'made' THEN 1 ELSE 0 END) as float))",
+          "col3"
+        ]
+      )
     })
   })
 })
