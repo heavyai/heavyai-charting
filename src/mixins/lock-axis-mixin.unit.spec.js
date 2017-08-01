b12d7d925376915070948f724aaead58c7d2d937
ʈᵃᵢ <tdupreetan@gmail.com>
2017-08-01 11:54:51 -0600

    adds stricter parsing of numbers for axis min/max (#84)
    * adds stricter parsing of numbers for axis min/max
    
    * trigger blur for IE

---
+++ src/mixins/lock-axis-mixin.unit.spec.js
@@ -0,0 +1,24 @@
+import mixin from "./lock-axis-mixin"
+import {expect} from "chai"
+
+describe("lock axis mixin", () => {
+  let chart
+
+  beforeEach(()=>{
+    chart = {
+      on: () => () => null,
+    }
+    chart = mixin(chart)
+  })
+  describe("constructor", () => {
+    it('should mixin a prepareLockAxis method', () => {
+      expect(typeof chart.prepareLockAxis).to.equal("function")
+    })
+    it("should create an elasticX event", (done) => {
+      chart.on("elasticX", function () {
+        done()
+      })
+      chart._invokeelasticXListener()
+    })
+  })
+})
