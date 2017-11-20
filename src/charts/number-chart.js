3676ce2ceab3f6321fac17f41411e684c151bb40
Jonathan Huang <grokut@gmail.com>
2017-11-20 17:22:04 -0500

    cache data for number chart
    cache number chart request

--- src/charts/number-chart.js
+++ src/charts/number-chart.js
@@ -1,6 +1,7 @@
 import baseMixin from "../mixins/base-mixin"
 import d3 from "d3"
 import {utils} from "../utils/utils"
+import {lastFilteredSize, setLastFilteredSize} from "../core/core-async"
 
 export default function numberChart (parent, chartGroup) {
   const _chart = baseMixin({})
@@ -28,38 +29,80 @@ export default function numberChart (parent, chartGroup) {
     return typeof _colors === "string" ? _colors : _colors[0]
   }
 
-  _chart.setDataAsync((group, callbacks) => group.valueAsync().then((data) => {
-    callbacks(null, data)
-  }).catch((error) => {
-    callbacks(error)
-  }))
+  _chart.setDataAsync((group, callbacks) =>
+    group
+      .valueAsync()
+      .then(data => {
+        if (group.getReduceExpression() === "COUNT(*) AS val") {
+          const id = group.getCrossfilterId()
+          const filterSize = lastFilteredSize(id)
+          if (filterSize !== undefined) {
+            return Promise.resolve(filterSize)
+          } else {
+            return _chart
+              .dimension()
+              .sizeAsync()
+              .then(group.valueAsync)
+              .then(value => {
+                setLastFilteredSize(id, value)
+                return value
+              })
+          }
+        } else {
+          return data
+        }
+      })
+      .then(data => {
+        callbacks(null, data)
+      })
+      .catch(error => {
+        callbacks(error)
+      })
+  )
 
   _chart._doRender = function (val) {
     const selected = utils.formatValue(val)
 
-    const wrapper = _chart.root().html("")
-            .append("div")
-            .attr("class", "number-chart-wrapper")
+    const wrapper = _chart
+      .root()
+      .html("")
+      .append("div")
+      .attr("class", "number-chart-wrapper")
 
-    wrapper.append("span")
-            .attr("class", "number-chart-number")
-            .style("color", _chart.getColor)
-            .style("font-size", (d) => Math.max(Math.floor(_chart.height() / 5), 32) + "px")
-            .html(selected === "-0" ? 0 : selected)
-            .style("font-size", function (d) {
-              const width = d3.select(this).node().getBoundingClientRect().width
-              let calcFontSize = parseInt(d3.select(this).node().style.fontSize.replace(/\D/g, ""))
+    wrapper
+      .append("span")
+      .attr("class", "number-chart-number")
+      .style("color", _chart.getColor)
+      .style(
+        "font-size",
+        d => Math.max(Math.floor(_chart.height() / 5), 32) + "px"
+      )
+      .html(selected === "-0" ? 0 : selected)
+      .style("font-size", function (d) {
+        const width = d3
+          .select(this)
+          .node()
+          .getBoundingClientRect().width
+        let calcFontSize = parseInt(
+          d3
+            .select(this)
+            .node()
+            .style.fontSize.replace(/\D/g, "")
+        )
 
-              if (width > _chart.width() - 64) {
-                calcFontSize = Math.max(calcFontSize * ((_chart.width() - 64) / width), 32)
-              }
+        if (width > _chart.width() - 64) {
+          calcFontSize = Math.max(
+            calcFontSize * ((_chart.width() - 64) / width),
+            32
+          )
+        }
 
-              _fontSize = !_fontSize || _chartWidth < _chart.width() ? calcFontSize : Math.min(_fontSize, calcFontSize)
+        _fontSize = !_fontSize || _chartWidth < _chart.width() ? calcFontSize : Math.min(_fontSize, calcFontSize)
 
-              _chartWidth = _chart.width()
+        _chartWidth = _chart.width()
 
-              return _fontSize + "px"
-            })
+        return _fontSize + "px"
+      })
 
     return _chart
   }
