6a1418a9eba3f21f5dd4983c3367785f398676c5
Chris Viau <chrisv@mapd.com>
2018-04-18 14:46:35 -0400

    fix scatterplot popup

--- src/charts/raster-chart.js
+++ src/charts/raster-chart.js
@@ -533,9 +533,11 @@ export default function rasterChart(parent, useMap, chartGroup, _mapboxgl) {
   _chart.measureValue = function(value, key) {
     const customFormatter = _chart.valueFormatter()
     // hack to undo the popup concatenation like "AVG(arrdelay)"
-    const keyTrimmed = key.replace(/.*\((.*)\).*/, "$1")
-    console.log(value, key)
-    return (customFormatter && customFormatter(value, keyTrimmed)) || value
+    let keyTrimmed = null
+    if (key) {
+       keyTrimmed = key.replace(/.*\((.*)\).*/, "$1")
+    }
+    return (keyTrimmed && customFormatter && customFormatter(value, keyTrimmed)) || value
   }
 
   _chart.displayPopup = function displayPopup(result, animate) {
