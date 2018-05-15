d26238f97234ff9fdce8e1896b5d58edeea01a68
uyanga-gb <uyanga-gb@users.noreply.github.com>
2018-05-15 09:53:43 -0700

    column filter on measure will update global filter (#208)
    * column filter on measure will update global filter
    
    * minor change
    
    * apply table column filter clear to global filter icon
    
    * white space fix

--- src/mixins/base-mixin.js
+++ src/mixins/base-mixin.js
@@ -1366,7 +1366,11 @@ export default function baseMixin(_chart) {
    * @param {*} datum
    */
   _chart.onClick = function(datum) {
-    const filter = _chart.keyAccessor()(datum)
+    // filtering on dimension will have key, but for filtering on measures which is on column doesn't. Thus, the filter is the column value only
+    const filter =
+      _chart.keyAccessor()(datum).length > 0
+        ? _chart.keyAccessor()(datum)
+        : datum
     _chart.handleFilterClick(d3.event, filter)
   }
 
