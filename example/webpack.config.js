0cf9147fe3c8a8b480977589085a9184fe138966
Chris Henrick <chrishenrick@gmail.com>
2018-06-14 17:05:46 -0700

    use babel-polyfill for examples

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -4,6 +4,7 @@ var path = require("path");
 module.exports = {
   entry: {
     app: [
+      "babel-polyfill",
       "script-loader!@mapd/connector/dist/browser-connector.js",
       "script-loader!@mapd/crossfilter/dist/mapd-crossfilter.js",
       "script-loader!mapd3/dist/mapd3.js",
