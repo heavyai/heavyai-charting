5b079dd2927bb83c3c86ee8312664c223610dbd1
Christopher Root <chris@mapd.com>
2018-10-08 12:37:25 -0700

    Adding line accumulation tests

--- example/vega_tests/accumulationTest.js
+++ example/vega_tests/accumulationTest.js
@@ -195,7 +195,7 @@ document.addEventListener("DOMContentLoaded", () => {
       {
         "name": "pointmap_fillColor",
         "type": "linear",
-        "domain": [ 0, 1 ],
+        "domain": [ 0.001, 0.999 ],
         "range": [ "rgba(17,95,154,0.475)", "rgba(208,244,0,0.85)" ],
         "accumulator": "density",
         "minDensityCnt": "min",
@@ -250,11 +250,11 @@ document.addEventListener("DOMContentLoaded", () => {
       {
         "name": "pointmap_fillColor",
         "type": "linear",
-        "domain": [ 0, 1 ],
+        "domain": [ 0.001, 0.999 ],
         "range": [ "rgba(17,95,154,0.475)", "rgba(208,244,0,0.85)" ],
         "accumulator": "density",
-        "minDensityCnt": "min",
-        "maxDensityCnt": "max",
+        "minDensityCnt": "-2ndStdDev",
+        "maxDensityCnt": "2ndStdDev",
         "clamp": true
       }
     ],
@@ -1281,6 +1281,254 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   ]
 
+  const lineAccumulationVegas = [
+    {
+      "width": 1052,
+      "height": 1057,
+      "data": [
+        {
+          "name": "pointmap",
+          "sql": "SELECT mapd_geo, rowid FROM north_america_rivers WHERE NOT(ST_XMin(mapd_geo) > -27.622867061127266 OR ST_XMax(mapd_geo) < -179.99999999999872 OR ST_YMin(mapd_geo) > 83.15921305514283 OR ST_YMax(mapd_geo) < 8.287980554435876) LIMIT 20000",
+          "format": {
+            "type": "lines",
+            "coords": {"x": ["mapd_geo"],"y": [{"from": "mapd_geo"}]},
+            "layout": "interleaved"
+          }
+        }
+      ],
+      "projections": [
+        {
+          "name": "projection",
+          "type": "mercator",
+          "bounds": {
+            "x": [-179.99999999999872,-27.622867061127266],
+            "y": [8.287980554435876,83.15921305514283]
+          }
+        }
+      ],
+      "scales": [
+        {
+          "name": "linecolor",
+          "type": "linear",
+          "domain": [0.001,0.999],
+          "range": ["blue","red"],
+          "accumulator": "density",
+          "minDensityCnt": "min",
+          "maxDensityCnt": "max"
+        }
+      ],
+      "marks": [
+        {
+          "type": "lines",
+          "from": {"data": "pointmap"},
+          "properties": {
+            "x": {"field": "x"},
+            "y": {"field": "y"},
+            "strokeColor": {"scale": "linecolor","value": 0},
+            "strokeWidth": 5,
+            "lineJoin": "round"
+          },
+          "transform": {"projection": "projection"}
+        }
+      ]
+    },
+    {
+      "width": 1052,
+      "height": 1057,
+      "data": [
+        {
+          "name": "pointmap",
+          "sql": "SELECT mapd_geo, scalerank, rowid FROM north_america_rivers WHERE NOT(ST_XMin(mapd_geo) > -27.622867061127266 OR ST_XMax(mapd_geo) < -179.99999999999872 OR ST_YMin(mapd_geo) > 83.15921305514283 OR ST_YMax(mapd_geo) < 8.287980554435876) LIMIT 20000",
+          "format": {
+            "type": "lines",
+            "coords": {"x": ["mapd_geo"],"y": [{"from": "mapd_geo"}]},
+            "layout": "interleaved"
+          }
+        }
+      ],
+      "projections": [
+        {
+          "name": "projection",
+          "type": "mercator",
+          "bounds": {
+            "x": [-179.99999999999872,-27.622867061127266],
+            "y": [8.287980554435876,83.15921305514283]
+          }
+        }
+      ],
+      "scales": [
+        {
+          "name": "linecolor",
+          "type": "ordinal",
+          "domain": [10,11,12],
+          "range": ["blue","green","red"],
+          "accumulator": "blend"
+        }
+      ],
+      "marks": [
+        {
+          "type": "lines",
+          "from": {"data": "pointmap"},
+          "properties": {
+            "x": {"field": "x"},
+            "y": {"field": "y"},
+            "strokeColor": {"scale": "linecolor","field": "scalerank"},
+            "strokeWidth": 5,
+            "lineJoin": "round"
+          },
+          "transform": {"projection": "projection"}
+        }
+      ]
+    },
+    {
+      "width": 1052,
+      "height": 1057,
+      "data": [
+        {
+          "name": "pointmap",
+          "sql": "SELECT origin_lon as x1, origin_lat as y1, dest_lon as x2, dest_lat as y2, flights.rowid FROM flights WHERE ((origin_lon >= -126.11384979251031 AND origin_lon <= -66.75320151428946) AND (origin_lat >= 11.908856927823408 AND origin_lat <= 58.0353230071384)) OR ((dest_lon >= -126.11384979251031 AND dest_lon <= -66.75320151428946) AND (dest_lat >= 11.908856927823408 AND dest_lat <= 58.0353230071384)) LIMIT 20000",
+          "format": {
+            "type": "lines",
+            "coords": {"x": ["x1","x2"],"y": ["y1","y2"]},
+            "layout": "interleaved"
+          }
+        }
+      ],
+      "projections": [
+        {
+          "name": "projection",
+          "type": "mercator",
+          "bounds": {
+            "x": [-126.11384979251031,-66.75320151428946],
+            "y": [11.908856927823408,58.0353230071384]
+          }
+        }
+      ],
+      "scales": [
+        {
+          "name": "linecolor",
+          "type": "linear",
+          "domain": [0.001,0.999],
+          "range": ["blue","red"],
+          "accumulator": "density",
+          "minDensityCnt": "-2ndStdDev",
+          "maxDensityCnt": "2ndStdDev"
+        }
+      ],
+      "marks": [
+        {
+          "type": "lines",
+          "from": {"data": "pointmap"},
+          "properties": {
+            "x": {"field": "x"},
+            "y": {"field": "y"},
+            "strokeColor": {"scale": "linecolor","value": 0},
+            "strokeWidth": 5
+          },
+          "transform": {"projection": "projection"}
+        }
+      ]
+    },
+    {
+      "width": 1052,
+      "height": 1057,
+      "data": [
+        {
+          "name": "pointmap",
+          "sql": "SELECT origin_lon as x1, origin_lat as y1, dest_lon as x2, dest_lat as y2, carrier_name, rowid FROM flights WHERE ((origin_lon >= -130.704671914419 AND origin_lon <= -63.48944119720274) AND (origin_lat >= 8.161828727775116 AND origin_lat <= 60.13379788749944)) OR ((dest_lon >= -130.704671914419 AND dest_lon <= -63.48944119720274) AND (dest_lat >= 8.161828727775116 AND dest_lat <= 60.13379788749944)) LIMIT 200000",
+          "format": {
+            "type": "lines",
+            "coords": {"x": ["x1","x2"],"y": ["y1","y2"]},
+            "layout": "interleaved"
+          }
+        }
+      ],
+      "projections": [
+        {
+          "name": "projection",
+          "type": "mercator",
+          "bounds": {
+            "x": [-130.704671914419,-63.48944119720274],
+            "y": [8.161828727775116,60.13379788749944]
+          }
+        }
+      ],
+      "scales": [
+        {
+          "name": "linecolor",
+          "type": "threshold",
+          "domain": [0.25,0.5,0.75],
+          "range": ["blue","green","red","orange"],
+          "accumulator": "pct",
+          "pctCategory": "American Airlines"
+        }
+      ],
+      "marks": [
+        {
+          "type": "lines",
+          "from": {"data": "pointmap"},
+          "properties": {
+            "x": {"field": "x"},
+            "y": {"field": "y"},
+            "strokeColor": {"scale": "linecolor","field": "carrier_name"},
+            "strokeWidth": 5
+          },
+          "transform": {"projection": "projection"}
+        }
+      ]
+    },
+    {
+      "width": 1052,
+      "height": 1057,
+      "data": [
+        {
+          "name": "pointmap",
+          "sql": "SELECT mapd_geo, rowid FROM kaggle_taxi_waypoints WHERE NOT(ST_XMin(mapd_geo) > -8.58182575664381 OR ST_XMax(mapd_geo) < -8.657827016599725 OR ST_YMin(mapd_geo) > 41.18795342561697 OR ST_YMax(mapd_geo) < 41.130461364579475) LIMIT 20000",
+          "format": {
+            "type": "lines",
+            "coords": {"x": ["mapd_geo"],"y": [{"from": "mapd_geo"}]},
+            "layout": "interleaved"
+          }
+        }
+      ],
+      "projections": [
+        {
+          "name": "projection",
+          "type": "mercator",
+          "bounds": {
+            "x": [-8.657827016599725,-8.58182575664381],
+            "y": [41.130461364579475,41.18795342561697]
+          }
+        }
+      ],
+      "scales": [
+        {
+          "name": "linecolor",
+          "type": "linear",
+          "domain": [0.001,0.999],
+          "range": ["blue","red"],
+          "accumulator": "density",
+          "minDensityCnt": "-2ndStdDev",
+          "maxDensityCnt": "2ndStdDev"
+        }
+      ],
+      "marks": [
+        {
+          "type": "lines",
+          "from": {"data": "pointmap"},
+          "properties": {
+            "x": {"field": "x"},
+            "y": {"field": "y"},
+            "strokeColor": {"scale": "linecolor","value": 0},
+            "strokeWidth": 5,
+            "lineJoin": "round"
+          },
+          "transform": {"projection": "projection"}
+        }
+      ]
+    }
+  ]
+
   new MapdCon()
     .protocol("http")
     .host("localhost")
@@ -1325,6 +1573,12 @@ document.addEventListener("DOMContentLoaded", () => {
         } catch (e) {
           console.log(e)
         }
+      })
+
+      lineAccumulationVegas.forEach((line_accum_vega) => { // these should all run without error
+          results = con.renderVega(1, JSON.stringify(line_accum_vega))
+          blobUrl = "data:image/png;base64," + results.image
+          w.document.write("<img src='" + blobUrl + "' alt='backend-rendered png'/>")
       })
     })
 })
