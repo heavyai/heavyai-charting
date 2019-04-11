33b0a7cd03479390a192b44d6b517214e1587607
uyanga-gb <uyanga.gb@gmail.com>
2019-04-11 16:23:39 -0700

    updating metis version

--- dist/mapdc.js
+++ dist/mapdc.js
@@ -49850,6 +49850,7 @@ function parseResolvefilter(sql, transform) {
 
 var GOLDEN_RATIO = 2654435761;
 
+var THIRTY_ONE_BITS = 2147483648;
 var THIRTY_TWO_BITS = 4294967296;
 
 function sample(sql, transform) {
@@ -49860,14 +49861,14 @@ function sample(sql, transform) {
   var threshold = Math.floor(THIRTY_TWO_BITS * ratio);
 
   if (transform.method === "multiplicativeRowid" && ratio < 1) {
-    sql.where.push("((MOD( MOD (" + transform.field + ", " + THIRTY_TWO_BITS + ") * " + GOLDEN_RATIO + " , " + THIRTY_TWO_BITS + ") < " + threshold + ") OR (" + transform.field + " IN (" + transform.expr.join(", ") + ")))");
+    sql.where.push("((MOD( MOD (" + transform.field + ", " + THIRTY_ONE_BITS + ") * " + GOLDEN_RATIO + " , " + THIRTY_TWO_BITS + ") < " + threshold + ") OR (" + transform.field + " IN (" + transform.expr.join(", ") + ")))");
   } else if (transform.method === "multiplicative" && ratio < 1) {
     // We are using simple modulo arithmetic expression conversion, 
     // (A * B) mod C = (A mod C * B mod C) mod C, 
     // to optimize the filter here. This helps  the overflow on the backend.
     // We don't have the full modulo expression for golden ratio since 
     // that is a constant expression and we can avoid that execution
-    sql.where.push("MOD( MOD (" + sql.from + ".rowid, " + THIRTY_TWO_BITS + ") * " + GOLDEN_RATIO + " , " + THIRTY_TWO_BITS + ") < " + threshold);
+    sql.where.push("MOD( MOD (" + sql.from + ".rowid, " + THIRTY_ONE_BITS + ") * " + GOLDEN_RATIO + " , " + THIRTY_TWO_BITS + ") < " + threshold);
   }
 
   return sql;
