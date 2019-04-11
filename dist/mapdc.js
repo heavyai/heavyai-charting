e19732aa9ef90222503103c0186a8e18f531c5b2
uyanga-gb <uyanga.gb@gmail.com>
2019-04-11 16:32:53 -0700

    updating mapd-data-layer-2 version to 0.0.14

--- dist/mapdc.js
+++ dist/mapdc.js
@@ -49793,7 +49793,7 @@ function parsePostFilter(sql, transform) {
       } else {
         operatorExpr = comparisonOperator(operator[transform.ops], transform.min, transform.max);
       }
-      var expr = transform.custom ? transform.fields[0] + " " + operatorExpr : transform.aggType + "(" + transform.table + "." + transform.fields[0] + ") " + operatorExpr;
+      var expr = transform.custom ? transform.fields[0] + " " + operatorExpr : transform.aggType + "(" + transform.fields[0] + ") " + operatorExpr;
       sql.having.push(expr);
     default:
       return sql;
