5357e80e49b2bb505629ff3f1ffc2b2e100120d3
Jim Thomason <jim@japanesewartuba.com>
2019-05-31 19:26:25 -0500

    [FE-8602] - bumps metis and crossfilter hashes (#340)
    * jimt/FE-8602 - bumps metis and crossfilter hashes
    
    * jimt/FE-8602 - updated build
    
    * jimt/FE-8602 - bumps crossfilter to master hash
    
    * jimt/FE-8602 updated yarn.lock

--- dist/mapdc.js
+++ dist/mapdc.js
@@ -49875,7 +49875,7 @@ function parseBin(sql, _ref) {
       extent = _ref.extent,
       maxbins = _ref.maxbins;
 
-  sql.select.push("cast((cast(" + field + " as float) - " + extent[0] + ") * " + maxbins / (extent[1] - extent[0]) + " as int) as " + as);
+  sql.select.push("case when\n      " + field + " >= " + extent[1] + "\n    then\n      " + (maxbins - 1) + "\n    else\n      cast((cast(" + field + " as float) - " + extent[0] + ") * " + maxbins / (extent[1] - extent[0]) + " as int)\n    end\n    as " + as);
   sql.where.push("((" + field + " >= " + extent[0] + " AND " + field + " <= " + extent[1] + ") OR (" + field + " IS NULL))");
   sql.having.push("(" + as + " >= 0 AND " + as + " < " + maxbins + " OR " + as + " IS NULL)");
   return sql;
