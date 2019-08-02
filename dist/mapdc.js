heavyai/heavyai-charting

commit e15f998ce37bd505cb688cf3c846f0afc1664c7b

[FE-8934] Fix hpt binning with colors in histogram (#370)
* Use millisecond resolution for Dates in multi-series-mixin

In the `processMultiSeriesResults()` function in `multi-series-mixin.js`, add a check to see if the values be used are Date objects. If so, explicitly call `toISOString()` instead of letting it implicitly call `toString()`, so that we retain millisecond precision.

This change should fix using HPT values on histogram with a color dimension. Previously, you would only get one bin, since `toString()` cuts off milliseconds. Now, we get the proper binning.

dist/mapdc.js
@@ -6081,7 +6081,9 @@ function baseMixin(_chart) {
   var _renderLabel = false;
 
   var _title = function _title(d) {
-    return _chart.keyAccessor()(d) + ": " + _chart.valueAccessor()(d);
+    var key = _chart.keyAccessor()(d);
+    var value = _chart.valueAccessor()(d);
+    return (key instanceof Date ? key.toISOString() : key) + ": " + (value instanceof Date ? value.toISOString() : value);
   };
   var _renderTitle = true;
   var _controlsUseVisibility = true;
@@ -9123,7 +9125,9 @@ function coordinateGridMixin(_chart) {
 
   function compareDomains(d1, d2) {
     return !d1 || !d2 || d1.length !== d2.length || d1.some(function (elem, i) {
-      return elem && d2[i] ? elem.toString() !== d2[i].toString() : elem === d2[i];
+      var elemString = elem instanceof Date ? elem.toISOString() : elem.toString();
+      var d2String = d2[i] instanceof Date ? d2[i].toISOString() : d2[i].toString();
+      return elem && d2[i] ? elemString !== d2String : elem === d2[i];
     });
   }
 
@@ -44584,8 +44588,9 @@ function processMultiSeriesResults(results) {
     if (Array.isArray(key0)) {
       var isExtract = key0[0].isExtract;
 
-      var min = isExtract ? key0[0].value : key0[0].value || key0[0];
-      var alias = key0[0].alias || min;
+      var rawMin = isExtract ? key0[0].value : key0[0].value || key0[0];
+      var min = rawMin instanceof Date ? rawMin.toISOString() : rawMin;
+      var alias = key0[0].alias instanceof Date ? key0[0].alias.toISOString() : key0[0].alias || min;
 
       if (typeof accum.ranges[alias] !== "number") {
         // eslint-disable-line no-negated-condition

src/mixins/coordinate-grid-mixin.js
@@ -503,8 +503,11 @@ export default function coordinateGridMixin (_chart) {
   function compareDomains (d1, d2) {
     return (
       !d1 || !d2 || d1.length !== d2.length || d1.some(
-        (elem, i) =>
-          elem && d2[i] ? elem.toString() !== d2[i].toString() : elem === d2[i]
+        (elem, i) => {
+          const elemString = elem instanceof Date ? elem.toISOString() : elem.toString()
+          const d2String = d2[i] instanceof Date ? d2[i].toISOString() : d2[i].toString()
+          return elem && d2[i] ? elemString !== d2String : elem === d2[i]
+        }
       )
     )
   }

src/mixins/base-mixin.js
@@ -93,7 +93,11 @@ export default function baseMixin(_chart) {
   let _renderLabel = false
 
   let _title = function(d) {
-    return _chart.keyAccessor()(d) + ": " + _chart.valueAccessor()(d)
+    const key = _chart.keyAccessor()(d)
+    const value = _chart.valueAccessor()(d)
+    return `${key instanceof Date ? key.toISOString() : key}: ${
+      value instanceof Date ? value.toISOString() : value
+    }`
   }
   let _renderTitle = true
   let _controlsUseVisibility = true

src/mixins/multi-series-mixin.js
@@ -18,8 +18,12 @@ function processMultiSeriesResults(results) {
 
       if (Array.isArray(key0)) {
         const { isExtract } = key0[0]
-        const min = isExtract ? key0[0].value : key0[0].value || key0[0]
-        const alias = key0[0].alias || min
+        const rawMin = isExtract ? key0[0].value : key0[0].value || key0[0]
+        const min = rawMin instanceof Date ? rawMin.toISOString() : rawMin
+        const alias =
+          key0[0].alias instanceof Date
+            ? key0[0].alias.toISOString()
+            : key0[0].alias || min
 
         if (typeof accum.ranges[alias] !== "number") {
           // eslint-disable-line no-negated-condition

commit 5454a7699cf518109981c9a92aadf1d67c419fce

Handle invalid Date objects in `compareDomains()`
In certain circumstances (like creating a date global filter, and just clicking inside the start/end time inputs without changing them), the Date objects in `d2` (the second domain array) are invalid. Like, they're Date objects, but were created with invalid values. I'm not sure how or why these invalid Date objects are being created, but nonetheless, calling `toISOString()` on them throws an exception.

Due to JavaScript being JavaScript, calling `toString()` does not throw an exception; it just returns the string "Invalid date". So in `compareDomains()`, we test if the Date is valid (by checking `getTime()` doesn't return NaN). If it's valid, we call `toISOString()` like normal; if it's invalid, we just call `toString()` to avoid the exception.

This bug was introduced by PR #370 (FE-8934), which changed `toString()` to `toISOString()` in `compareDomains()`.

dist/mapdc.js
@@ -9125,8 +9125,11 @@ function coordinateGridMixin(_chart) {
 
   function compareDomains(d1, d2) {
     return !d1 || !d2 || d1.length !== d2.length || d1.some(function (elem, i) {
-      var elemString = elem instanceof Date ? elem.toISOString() : elem.toString();
-      var d2String = d2[i] instanceof Date ? d2[i].toISOString() : d2[i].toString();
+      // The date objects here may be invalid for some reason. We detect that by ensuring
+      // getTime() doesn't return NaN. If invalid, we use toString() instead of toISOString,
+      // since the former doesn't throw an exception with invalid dates.
+      var elemString = elem instanceof Date && !isNaN(elem.getTime()) ? elem.toISOString() : elem.toString();
+      var d2String = d2[i] instanceof Date && !isNaN(d2[i].getTime()) ? d2[i].toISOString() : d2[i].toString();
       return elem && d2[i] ? elemString !== d2String : elem === d2[i];
     });
   }
@@ -11226,7 +11229,7 @@ function getScales(_ref, layerName, scaleDomainFields, xformDataSource) {
   var undefined;
 
   /** Used as the semantic version number. */
-  var VERSION = '4.17.14';
+  var VERSION = '4.17.15';
 
   /** Used as the size to enable large array optimizations. */
   var LARGE_ARRAY_SIZE = 200;

src/mixins/coordinate-grid-mixin.js
@@ -504,8 +504,11 @@ export default function coordinateGridMixin (_chart) {
     return (
       !d1 || !d2 || d1.length !== d2.length || d1.some(
         (elem, i) => {
-          const elemString = elem instanceof Date ? elem.toISOString() : elem.toString()
-          const d2String = d2[i] instanceof Date ? d2[i].toISOString() : d2[i].toString()
+          // The date objects here may be invalid for some reason. We detect that by ensuring
+          // getTime() doesn't return NaN. If invalid, we use toString() instead of toISOString,
+          // since the former doesn't throw an exception with invalid dates.
+          const elemString = elem instanceof Date && !isNaN(elem.getTime()) ? elem.toISOString() : elem.toString()
+          const d2String = d2[i] instanceof Date && !isNaN(d2[i].getTime()) ? d2[i].toISOString() : d2[i].toString()
           return elem && d2[i] ? elemString !== d2String : elem === d2[i]
         }
       )