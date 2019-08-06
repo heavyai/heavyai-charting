9b6fd16bf4fe97e984961525368e7185fd3b8ff6
Caitlin <caitlin.wolters@omnisci.com>
2019-08-06 13:53:48 -0500

    no longer needed

--- src/utils/mapbox-ported-functions.js
+++ src/utils/mapbox-ported-functions.js
@@ -5,64 +5,6 @@
  * - See [FE-8035]
  */
 
-const base64ToArrayBuffer = function(base64) {
-  // from SO: http://bit.ly/2fGowUT
-  const binaryString = window.atob(base64)
-  const len = binaryString.length
-  const bytes = new Uint8Array(len)
-  for (let i = 0; i < len; i++) {
-    bytes[i] = binaryString.charCodeAt(i)
-  }
-  return bytes.buffer
-}
-
-const getArrayBuffer = function(url, callback) {
-  if (url.slice(0, 22) === "data:image/png;base64,") {
-    return callback(null, base64ToArrayBuffer(url.slice(22)))
-  } else {
-    const xhr = new XMLHttpRequest()
-    xhr.open("GET", url, true)
-    xhr.responseType = "arraybuffer"
-    xhr.onerror = function(e) {
-      callback(e)
-    }
-    xhr.onload = function() {
-      if (xhr.status >= 200 && xhr.status < 300 && xhr.response) {
-        callback(null, xhr.response)
-      } else {
-        callback(new Error(xhr.statusText))
-      }
-    }
-    xhr.send()
-    return xhr
-  }
-}
-
-export const getImage = function(url, callback) {
-  return getArrayBuffer(url, (err, imgData) => {
-    if (err) {
-      return callback(err)
-    }
-    const img = new window.Image()
-    img.onload = () => {
-      callback(null, img)
-      ;(window.URL || window.webkitURL).revokeObjectURL(img.src)
-    }
-
-    const blob = new Blob([new Uint8Array(imgData)], { type: "image/png" })
-    img.src = (window.URL || window.webkitURL).createObjectURL(blob)
-    img.getData = () => {
-      const canvas = document.createElement("canvas")
-      const context = canvas.getContext("2d")
-      canvas.width = img.width
-      canvas.height = img.height
-      context.drawImage(img, 0, 0)
-      return context.getImageData(0, 0, img.width, img.height).data
-    }
-    return img
-  })
-}
-
 /**
  * Provides a function that outputs milliseconds: either performance.now()
  * or a fallback to Date.now()
