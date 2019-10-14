5bd06e105aaafb611c6835c27ce5618d2282e620
uyanga-gb <uyanga-gb@users.noreply.github.com>
2019-10-14 11:47:51 -0700

    [FE-9522] temporarily disable the new mapbox mouse events (#379)
    * temporarily disable the new mapbox mouse events
    
    * running prettier

--- src/mixins/map-mixin.js
+++ src/mixins/map-mixin.js
@@ -604,8 +604,6 @@ export default function mapMixin(
 
   _chart.addMapListeners = function() {
     _map.on("move", onMapMove)
-    _map.on("drag", onMapMove)
-    _map.on("wheel", onMapMove)
     _map.on("moveend", onMapMove)
     _map.on("sourcedata", showMapLogo)
   }
--- src/mixins/raster-draw-mixin.js
+++ src/mixins/raster-draw-mixin.js
@@ -588,15 +588,7 @@ export function rasterDrawMixin(chart) {
     if (typeof chart.useLonLat === "function") {
       // using a mapbox map, it works better to rerender
       // on move here
-      chart.map().on("drag", updateDraw)
-      chart.map().on("wheel", updateDraw)
-
-      // TODO need to find right original event listener to update
-      chart.map().on("move", e => {
-        if (e.target && e.target._zooming) {
-          updateDraw()
-        }
-      })
+      chart.map().on("move", updateDraw)
     } else {
       // using a dc coordinate grid, redraws work better
       // on the render event
