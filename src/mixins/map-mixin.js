370a6bba2415ed6e9468f127c51c1ca871f465ae
uyanga-gb <uyanga-gb@users.noreply.github.com>
2019-10-21 16:39:28 -0700

    [FE-9658] moved map scale to bottom right (#381)
    * moved map scale to bottom right
    
    * running prettier

--- src/mixins/map-mixin.js
+++ src/mixins/map-mixin.js
@@ -597,7 +597,8 @@ export default function mapMixin(
     _map.addControl(new _mapboxgl.NavigationControl(), "bottom-right")
     _map.addControl(new _mapboxgl.AttributionControl(), _attribLocation)
     _map.addControl(
-      new _mapboxgl.ScaleControl({ maxWidth: 80, unit: "metric" })
+      new _mapboxgl.ScaleControl({ maxWidth: 80, unit: "metric" }),
+      "bottom-right"
     )
     _chart.addMapListeners()
     _mapInitted = true
