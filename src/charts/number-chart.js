f44b7a5b157ccde5b572233e0572fc5dcb422982
Bob Matcuk <robert.matcuk@omnisci.com>
2019-11-05 17:47:53 -0500

    fix issue with number charts not updating (#390)

--- src/charts/number-chart.js
+++ src/charts/number-chart.js
@@ -25,26 +25,6 @@ export default function numberChart(parent, chartGroup) {
     group
       .valueAsync()
       .then(data => {
-        if (group.getReduceExpression() === "COUNT(*) AS val") {
-          const id = group.getCrossfilterId()
-          const filterSize = lastFilteredSize(id)
-          if (filterSize !== undefined) {
-            return Promise.resolve(filterSize)
-          } else {
-            return _chart
-              .dimension()
-              .sizeAsync()
-              .then(group.valueAsync)
-              .then(value => {
-                setLastFilteredSize(id, value)
-                return value
-              })
-          }
-        } else {
-          return data
-        }
-      })
-      .then(data => {
         callbacks(null, data)
       })
       .catch(error => {
