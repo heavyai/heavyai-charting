3f65ab9c3efd98ec99c32878f0c819d40b795660
Caitlin <caitlin.wolters@omnisci.com>
2020-01-22 18:31:52 -0800

    [FE-10291] Add range slider for bubble charts

--- dist/mapdc.js
+++ dist/mapdc.js
@@ -33098,6 +33098,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  */
 function bubbleMixin(_chart) {
   var _maxBubbleRelativeSize = 0.3;
+  var _minBubbleRelativeSize = 0.004;
 
   /* OVERRIDE ---------------------------------------------------------------- */
   var _minRadiusWithLabel = 2;
@@ -33316,6 +33317,14 @@ function bubbleMixin(_chart) {
       return _maxBubbleRelativeSize;
     }
     _maxBubbleRelativeSize = relativeSize;
+    return _chart;
+  };
+
+  _chart.minBubbleRelativeSize = function (relativeSize) {
+    if (!arguments.length) {
+      return _minBubbleRelativeSize;
+    }
+    _minBubbleRelativeSize = relativeSize;
     return _chart;
   };
 
@@ -69781,6 +69790,8 @@ function bubbleChart(parent, chartGroup) {
 
   var _elasticRadius = false;
   var _sortBubbleSize = false;
+  var _hasSizeMeasure = false;
+  var SMALLEST_SIZED_BUBBLE_RADIUS = 2;
 
   _chart.transitionDuration(750);
 
@@ -70024,6 +70035,14 @@ function bubbleChart(parent, chartGroup) {
     return _chart;
   };
 
+  _chart.hasSizeMeasure = function (hasSizeMeasure) {
+    if (!arguments.length) {
+      return _hasSizeMeasure;
+    }
+    _hasSizeMeasure = hasSizeMeasure;
+    return _chart;
+  };
+
   /**
    * Turn on or off the bubble sorting feature, or return the value of the flag. If enabled,
    * bubbles will be sorted by their radius, with smaller bubbles in front.
@@ -70047,7 +70066,22 @@ function bubbleChart(parent, chartGroup) {
       _chart.r().domain([_chart.rMin(), _chart.rMax()]);
     }
 
-    _chart.r().range([_chart.MIN_RADIUS, _chart.xAxisLength() * _chart.maxBubbleRelativeSize()]);
+    // If the chart has a size measure, we're going to render bubbles
+    // differently than if the chart doesn't have a size measure
+    if (_hasSizeMeasure) {
+      // If it has a size measure, we're going to scale the bubbles. This scale
+      // is a function of a multiplier and the chart width. We allow the user to
+      // select the multiplier for the lower and upper bounds of bubble radii.
+      // However, we want to enforce an absolute minimum radius of 2px on
+      // rendered bubbles.
+      var minimumBubbleSizeInPixels = Math.max(SMALLEST_SIZED_BUBBLE_RADIUS, _chart.xAxisLength() * _chart.minBubbleRelativeSize());
+      _chart.r().range([minimumBubbleSizeInPixels, _chart.xAxisLength() * _chart.maxBubbleRelativeSize()]);
+    } else {
+      // If we have a size measure, the bubble size will be a fixed radius
+      // determined by the user and stored in _chart.MIN_RADIUS. This fixed with
+      // is not responsive and will not vary with screen size.
+      _chart.r().range([_chart.MIN_RADIUS, _chart.MIN_RADIUS]);
+    }
 
     var data = _chart.data();
     if (_sortBubbleSize) {
