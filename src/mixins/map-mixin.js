855f7e788aeb42635987ef7aaa7a4416a4b1b26c
uyanga-gb <uyangaganbaatar@MacBook-Pro-4.local>
2020-06-09 14:53:43 -0700

    use single call to update zoom and center

--- src/mixins/map-mixin.js
+++ src/mixins/map-mixin.js
@@ -446,6 +446,20 @@ export default function mapMixin(
     return _chart
   }
 
+  _chart.flyTo = function(zoom, center) {
+    if (!arguments.length) {
+      _center = _map.getCenter()
+      return _center
+    }
+
+    _zoom = zoom
+    _center = center
+    if (_mapInitted) {
+      _map.flyTo({ center: _center, zoom: _zoom })
+    }
+    return _chart
+  }
+
   _chart.attribLocation = function(_) {
     if (!arguments.length) {
       return _attribLocation
