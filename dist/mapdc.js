5dd698bfea76e08ea28648b0400213a4c9ad00d7
Uyanga Ganbaatar <uyanga-gb@users.noreply.github.com>
2020-06-19 12:20:01 -0700

    support packed-pixel-coords_hex rec_map (#439)
    Co-authored-by: uyanga-gb <uyangaganbaatar@MacBook-Pro-4.local>

--- dist/mapdc.js
+++ dist/mapdc.js
@@ -5644,11 +5644,9 @@ function hexBinSQL(sql, _ref, parser) {
 
   var args = parser.parseExpression(x.field) + "," + (hexminmercx + ",") + (hexmaxmercx + ",") + (parser.parseExpression(y.field) + ",") + (hexminmercy + ",") + (hexmaxmercy + ",") + (mark.width + ",") + (mark.height + ",") + (hexoffsetx + ",") + (hexoffsety + ",") + (width + ",") + ("" + height);
 
-  sql.select.push("reg_" + mark.shape + "_horiz_pixel_bin_x(" + args + ") AS x");
-  sql.select.push("reg_" + mark.shape + "_horiz_pixel_bin_y(" + args + ") AS y");
+  sql.select.push("reg_" + mark.shape + "_horiz_pixel_bin_packed(" + args + ") AS xy");
   sql.select.push(parser.parseExpression(aggregate) + " AS color");
-  sql.groupby.push("x");
-  sql.groupby.push("y");
+  sql.groupby.push("xy");
 
   return sql;
 }
@@ -5661,11 +5659,10 @@ function rectBinSQL(sql, _ref2, parser) {
       y = _ref2.y,
       aggregate = _ref2.aggregate;
 
-  sql.select.push("rect_pixel_bin_x(" + parser.parseExpression(x.field) + ", " + x.domain[0] + ", " + x.domain[1] + ", " + mark.width + ", 0, " + width + ") AS x");
-  sql.select.push("rect_pixel_bin_y(" + parser.parseExpression(y.field) + ", " + y.domain[0] + ", " + y.domain[1] + ", " + mark.height + ", 0, " + height + ") AS y");
+  sql.select.push("rect_pixel_bin_packed(" + parser.parseExpression(x.field) + ", " + x.domain[0] + ", " + x.domain[1] + ", " + parser.parseExpression(y.field) + ", " + y.domain[0] + ", " + y.domain[1] + ", " + mark.width + ", " + mark.height + ", 0, 0, " + width + ", " + height + ") AS xy");
+
   sql.select.push(parser.parseExpression(aggregate) + " AS color");
-  sql.groupby.push("x");
-  sql.groupby.push("y");
+  sql.groupby.push("xy");
 
   return sql;
 }
@@ -53658,10 +53655,10 @@ function rasterLayerHeatmapMixin(_layer) {
       properties: {
         shape: getMarkType(state.mark),
         xc: {
-          field: "x"
+          field: "xy"
         },
         yc: {
-          field: "y"
+          field: "xy"
         },
         width: markWidth,
         height: markHeight,
