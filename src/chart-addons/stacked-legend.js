73035fb692348b03997f6e2197e00b3a59eb3f06
uyanga-gb <uyangaganbaatar@MacBook-Pro-4.local>
2021-01-26 21:42:17 -0800

    run format

--- src/chart-addons/stacked-legend.js
+++ src/chart-addons/stacked-legend.js
@@ -265,7 +265,7 @@ function legendState(state, useMap = true) {
       open: hasLegendOpenProp(state) ? state.legend.open : true,
       range:
         state.hasOwnProperty("showOther") && state.showOther === false
-          ? state.range.slice(0, state.range.length - 1) 
+          ? state.range.slice(0, state.range.length - 1)
           : state.range,
       domain: state.domain,
       position: useMap ? "bottom-left" : "top-right"
