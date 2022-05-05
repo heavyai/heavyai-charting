c59b92e41c8f274097bb1acc1407a0f91c87bda8
Chris Matzenbach <chris.matzenbach@omnisci.com>
2022-05-05 15:27:09 -0500

    [FE-15240] Try to debug unit tests that are failing on build but not locally...

--- src/utils/utils-vega.js
+++ src/utils/utils-vega.js
@@ -31,6 +31,8 @@ export function adjustRGBAOpacity(rgba, opacity) {
 }
 
 export function parseColorRamps(cr) {
+  console.log(cr)
+  console.log(typeof cr)
   if (cr) {
     const cr_arr = cr
       .flatMap(a => a.filter(b => b !== "min" && b !== "max" && b !== ""))
