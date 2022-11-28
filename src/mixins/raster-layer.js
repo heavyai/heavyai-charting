6beda6fc16488bbd68966b459ad136959644423e
Christopher Root <chris.root@heavy.ai>
2022-11-28 14:25:55 -0800

    Point mapd-draw to the latest commit. This required updating all mapd-draw - related import statements to pull from the unbranded draw bundle.

--- src/mixins/raster-layer.js
+++ src/mixins/raster-layer.js
@@ -9,7 +9,7 @@ import {
   createRasterLayerGetterSetter,
   createVegaAttrMixin
 } from "../utils/utils-vega"
-import { AABox2d, Point2d } from "@heavyai/draw/dist/mapd-draw"
+import { AABox2d, Point2d } from "@heavyai/draw/dist/draw"
 import moment from "moment"
 
 const validLayerTypes = [
