7f1286973ae819fbd30f9293345e4a2c690637b2
Christopher Root <chris.root@heavy.ai>
2022-11-29 18:03:01 -0800

    Some extra comments around the lasso shape events. Still could use some documentation on the data passed to event callbacks

--- src/mixins/ui/lasso-event-constants.js
+++ src/mixins/ui/lasso-event-constants.js
@@ -1,9 +1,39 @@
 export const LassoShapeEventConstants = {
+  /**
+   * triggered from an individual shape when a draggable edit
+   * event starts on that shape
+   */
   LASSO_SHAPE_EDIT_BEGIN: "lasso:shape:edit:begin",
+
+  /**
+   * triggered from an individual shape when a draggable edit
+   * event completes on that shape
+   */
   LASSO_SHAPE_EDIT_END: "lasso:shape:edit:end"
 }
 
 export const LassoGlobalEventConstants = {
+  /**
+   * triggered when a new lasso shape is created
+   */
   LASSO_SHAPE_CREATE: "lasso:shape:create",
-  LASSO_SHAPE_DESTROY: "lasso:shape:destroy"
+
+  /**
+   * triggered when a lasso shape is destroyed
+   */
+  LASSO_SHAPE_DESTROY: "lasso:shape:destroy",
+
+  /**
+   * triggered when a draggable edit event starts.
+   * The draggable event could be applied to one or many
+   * shapes
+   */
+  LASSO_SHAPE_EDITS_BEGIN: "lasso:shape:edits:begin",
+
+  /**
+   * triggered when a draggable edit event completes.
+   * The draggable event could be applied to one or many
+   * shapes
+   */
+  LASSO_SHAPE_EDITS_END: "lasso:shape:edits:end"
 }
