94bb9d82332358f1ff1432386967fcbdf2b89fe4
Chris Matzenbach <chris.matzenbach@omnisci.com>
2024-09-13 16:33:56 -0500

    Move all others to new function

--- src/charts/pie-chart.js
+++ src/charts/pie-chart.js
@@ -468,7 +468,28 @@ export default function pieChart(parent, chartGroup) {
       )
   }
 
+  function maybeUpdateAllOthers(data, domain, range) {
+    if (
+      data.map(d => d.data.key0).includes("All Others") &&
+      !domain.includes("All Others")
+    ) {
+      domain.push("All Others")
+      range.push("#888888")
+    } else if (
+      domain.includes("All Others") &&
+      !data.map(d => d.data.key0).includes("All Others")
+    ) {
+      const index = domain.indexOf("All Others")
+      domain.splice(index, 1)
+      range.splice(index, 1)
+    }
+
+    _chart.customDomain(domain)
+    _chart.customRange(range)
+  }
+
   function updateElements(pieData, arc) {
+    maybeUpdateAllOthers(pieData, _chart.customDomain(), _chart.customRange())
     const domain = _chart.customDomain()
     const range = _chart.customRange()
 
@@ -489,32 +510,6 @@ export default function pieChart(parent, chartGroup) {
       const newRange = filteredData.map((d, i) => _chart.getColor(d.data, i))
       _chart.customRange(newRange)
     } else if (domain.length > 0) {
-      // if all others is present in data but not customDomain
-      if (filteredData.length > domain.length) {
-        const missingData = pieData.filter(d => !domain.includes(d.data.key0))
-        if (
-          missingData.length > 0 &&
-          missingData[0].data.key0 === "All Others"
-        ) {
-          domain.push(missingData[0].data.key0)
-          range.push("#888888")
-          _chart.customDomain(domain)
-          _chart.customRange(range)
-        }
-      } else if (domain.length > filteredData.length) {
-        // if all others is present in custom domain but not in data
-        console.log(filteredData.map(d => d.data.key0))
-        if (
-          domain.includes("All Others") &&
-          !filteredData.map(d => d.data.key0).includes("All Others")
-        ) {
-          const index = domain.indexOf("All Others")
-          domain.splice(index, 1)
-          range.splice(index, 1)
-          _chart.customDomain(domain)
-          _chart.customRange(range)
-        }
-      }
       // if we have custom domain and range, filter fetched data
       filteredData = pieData.filter(d => domain.includes(d.data.key0))
     }
