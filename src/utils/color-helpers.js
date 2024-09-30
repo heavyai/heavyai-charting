fb0847ddde33f84e1a4f523944eed0c116efd82d
Chris Matzenbach <chris.matzenbach@omnisci.com>
2024-09-30 12:59:36 -0500

    Refactor per review comments

--- src/utils/color-helpers.js
+++ src/utils/color-helpers.js
@@ -10,29 +10,25 @@ export function maybeUpdateDomainRange(
   wrapKey = false
 ) {
   const dataArr = data.map(dataAccessor)
-  const domainRangeMap = new Map([...domain].map((key, i) => [key, range[i]]))
+  const domainRangeMap = new Map(domain.map((key, i) => [key, range[i]]))
   const dataMap = new Map(
-    [...dataArr].map((key, i) => [
+    dataArr.map((key, i) => [
       key,
       domainRangeMap.get(key) ??
         chart.getColor(wrapKey ? { key0: key } : key, i)
     ])
   )
-  chart.customDomain([...dataMap.keys()])
-  chart.customRange([...dataMap.values()])
+  chart.customDomain(dataMap.keys())
+  chart.customRange(dataMap.values())
 }
 
 export function maybeUpdateAllOthers(chart, data, domain, range) {
-  if (
-    data.map(d => d.data.key0).includes(ALL_OTHERS_LABEL) &&
-    !domain.includes(ALL_OTHERS_LABEL)
-  ) {
+  const dataHasAllOthers = data.map(d => d.data.key0).includes(ALL_OTHERS_LABEL)
+  const domainHasAllOthers = domain.includes(ALL_OTHERS_LABEL)
+  if (dataHasAllOthers && !domainHasAllOthers) {
     domain.push(ALL_OTHERS_LABEL)
     range.push(ALL_OTHERS_COLOR)
-  } else if (
-    domain.includes(ALL_OTHERS_LABEL) &&
-    !data.map(d => d.data.key0).includes(ALL_OTHERS_LABEL)
-  ) {
+  } else if (domainHasAllOthers && !dataHasAllOthers) {
     const index = domain.indexOf(ALL_OTHERS_LABEL)
     domain.splice(index, 1)
     range.splice(index, 1)
